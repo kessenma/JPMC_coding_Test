f495035ce1487eff01c561e9db66bf46061a4963
Kyle Essenmacher <kessen@umich.edu>
2022-07-07 01:05:49 -0400

    added in more rows of schools, still couldn't get the SAT View to work

---
+++ SatView.swift
@@ -0,0 +1,20 @@
+//
+//  SatView.swift
+//  NYCSchools
+//
+//  Created by Kyle Essenmacher on 7/6/22.
+//
+
+import SwiftUI
+
+struct SatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SatView_Previews: PreviewProvider {
+    static var previews: some View {
+        SatView()
+    }
+}
